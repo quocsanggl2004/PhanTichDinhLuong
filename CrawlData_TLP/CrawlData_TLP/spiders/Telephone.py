4c41e7a7840dfcdb624b9551d2ee5bee01f3923b
foxxiee04 <sangtranquocgl@gmail.com>
2024-09-17 22:52:59 +0700

    Xonggggg

--- CrawlData_TLP/CrawlData_TLP/spiders/Telephone.py
+++ CrawlData_TLP/CrawlData_TLP/spiders/Telephone.py
@@ -1,5 +1,6 @@
 import scrapy
 import json
+import re  
 
 class CellphonesGraphQLSpider(scrapy.Spider):
     name = "Telephone"
@@ -67,35 +68,75 @@ class CellphonesGraphQLSpider(scrapy.Spider):
             meta={'page': 1}
         )
 
+
     def parse_products(self, response):
         data = json.loads(response.text)
         products = data.get('data', {}).get('products', [])
+
+        # Hàm loại bỏ các thẻ HTML
+        def remove_html_tags(text):
+            clean = re.compile('<.*?>')
+            return re.sub(clean, ' ', text)
+
+
+        # Hàm xử lý văn bản
+        def clean_text(text):
+            if text:
+                text = text.strip()  # Loại bỏ khoảng trắng đầu và cuối
+                text = re.sub(r'\s+', ' ', text)  # Loại bỏ khoảng trắng thừa giữa các từ
+            return text
+        
 
         # Lưu từng sản phẩm
         for product in products:
             general_info = product.get('general', {})
             filterable_info = product.get('filterable', {})
-
+            attributes = general_info.get('attributes', {})
+        
+            name = clean_text(general_info.get('name', 'N/A'))
+        
+            mobile_tinh_nang_camera_raw = attributes.get('mobile_tinh_nang_camera', 'N/A')
+            mobile_tinh_nang_camera = clean_text(remove_html_tags(mobile_tinh_nang_camera_raw))
+        
+            mobile_cong_nghe_sac_raw = attributes.get('mobile_cong_nghe_sac', 'N/A')
+            mobile_cong_nghe_sac = clean_text(remove_html_tags(mobile_cong_nghe_sac_raw))
+        
+            # Định dạng average_rating
+            average_rating = general_info.get('review', {}).get('average_rating')
+            if average_rating:
+                formatted_rating = f"{average_rating}/5"
+            else:
+                formatted_rating = 'N/A'
+        
+            # Định dạng camera_primary và camera_secondary
+            camera_primary = remove_html_tags(attributes.get('camera_primary', 'N/A'))
+            camera_secondary = remove_html_tags(attributes.get('camera_secondary', 'N/A'))
+        
             yield {
                 'product_id': general_info.get('product_id'),
-                'name': general_info.get('name'),
-                'attributes': general_info.get('attributes'),
-                'sku': general_info.get('sku'),
-                'manufacturer': general_info.get('manufacturer'),
+                'name': name, 
                 'price': filterable_info.get('price'),
                 'special_price': filterable_info.get('special_price'),
-                'promotion': filterable_info.get('promotion_information'),
-                'thumbnail': filterable_info.get('thumbnail'),
-                'review_count': general_info.get('review', {}).get('total_count'),
-                'average_rating': general_info.get('review', {}).get('average_rating'),
+                'battery': clean_text(attributes.get('battery', 'N/A')),
+                'display_size': clean_text(attributes.get('display_size', 'N/A')),
+                'mobile_tan_so_quet': clean_text(attributes.get('mobile_tan_so_quet', 'N/A')),
+                'manufacturer': clean_text(general_info.get('manufacturer', 'N/A')),
+                'camera_primary': camera_primary,  
+                'camera_secondary': camera_secondary,  
+                'mobile_tinh_nang_camera': mobile_tinh_nang_camera,  
+                'chipset': clean_text(attributes.get('chipset', 'N/A')),
+                'mobile_tinh_nang_dac_biet': clean_text(attributes.get('mobile_tinh_nang_dac_biet', 'N/A')),
+                'mobile_cong_nghe_sac': mobile_cong_nghe_sac,  
+                'mobile_cong_sac': clean_text(attributes.get('mobile_cong_sac', 'N/A')),
+                'warranty_information': clean_text(attributes.get('warranty_information', 'N/A')),
+                'average_rating': formatted_rating,  
             }
 
         # Chuyển sang trang tiếp theo nếu còn sản phẩm
         current_page = response.meta.get('page')
-        if products:  # Nếu còn sản phẩm trong trang hiện tại
+        if products:
             next_page = current_page + 1
 
-            # Cập nhật payload cho trang tiếp theo
             next_payload = {
                 "query": """
                 query GetProductsByCateId{
@@ -139,7 +180,6 @@ class CellphonesGraphQLSpider(scrapy.Spider):
                 "variables": {}
             }
 
-            # Gửi yêu cầu cho trang tiếp theo
             yield scrapy.Request(
                 url=self.start_urls[0],
                 method="POST",
@@ -149,8 +189,9 @@ class CellphonesGraphQLSpider(scrapy.Spider):
                 meta={'page': next_page}
             )
 
+
     def parse(self, response):
-        pass  # Không sử dụng phương thức này, nhưng Scrapy yêu cầu phải có.
+        pass  
 
 
     # def parse_product_details(self, response):
@@ -180,17 +221,17 @@ class CellphonesGraphQLSpider(scrapy.Spider):
     #     yield item
 
 
-    def extract_promotion_text(self, product):
-        parts = product.css('div.promotion p::text').getall()
-        promotion_text = ' '.join(part.strip() for part in parts)
-        return promotion_text
+    # def extract_promotion_text(self, product):
+    #     parts = product.css('div.promotion p::text').getall()
+    #     promotion_text = ' '.join(part.strip() for part in parts)
+    #     return promotion_text
 
-    def clean_text(self, text):
-        if text:
-            text = text.replace('\xa0', ' ')
-            return ' '.join(text.split())
-        return text
+    # def clean_text(self, text):
+    #     if text:
+    #         text = text.replace('\xa0', ' ')
+    #         return ' '.join(text.split())
+    #     return text
 
-    def close(self, reason):
-        # Đóng trình điều khiển Selenium khi kết thúc
-        self.driver.quit()
+    # def close(self, reason):
+    #     # Đóng trình điều khiển Selenium khi kết thúc
+    #     self.driver.quit()
