a1a0e6d25105765a119a7590c67298e4786d9da8
foxxiee04 <sangtranquocgl@gmail.com>
2024-09-17 17:44:27 +0700

    Sap xong

---
+++ items.py
@@ -0,0 +1,22 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class CrawldataV5Item(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    product_id = scrapy.Field()
+    name = scrapy.Field()
+    attributes = scrapy.Field()
+    sku = scrapy.Field()
+    manufacturer = scrapy.Field()
+    price = scrapy.Field()
+    special_price = scrapy.Field()  
+    promotion = scrapy.Field() 
+    thumbnail = scrapy.Field()  
+    review_count = scrapy.Field()
+    average_rating = scrapy.Field()  
